f899bbe775d6ff454d8e9f9dcced48bd2ffb7be6
David Stewart <darkorin00@hotmail.com>
2020-05-27 20:43:57 -0400

    added inquirer prompts and got basics working

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ inquirer.prompt([{
     message: "Enter repo name",
     name: "repo"
 }]).then(({ username, repo }) => {
+    let description;
     let installation;
     let usage;
     let tests;
@@ -22,13 +23,15 @@ inquirer.prompt([{
         .then(res => {
             const writeReadMe = () => {
                 // Forces it to wait until all the various functions are finished before writed the readme file.
-                console.log(installation != undefined && usage != undefined && tests != undefined && faq != undefined && credits != undefined && license != undefined)
-                if(installation != undefined && usage != undefined && tests != undefined && faq != undefined && credits != undefined && license != undefined)
+                if(description != undefined && installation != undefined && usage != undefined && tests != undefined && faq != undefined && credits != undefined && license != undefined)
                 {
                     const readme =
 `
 # ${repo}
 # Author: ${username}
+
+${description}
+
 ![size](https://img.shields.io/github/repo-size/${username}/${repo})
         
 # Table of Contents:
@@ -60,21 +63,55 @@ ${license}
                     fs.writeFile(`${repo} README.md`, readme.trim(), err => { if (err) console.log(err) });
                 }
             };
+            const writeDescription = (des) => {
+                if (des != null) {
+                    description = des;
+                    writeInstallation();
+                } else {
+                    inquirer.prompt({
+                        message: "Write a short description about your project.",
+                        name: "desc" 
+                    }).then(({desc}) => {
+                        description = desc;
+                        writeInstallation();
+                    })
+                }       
+            }
             const writeInstallation = () => {
-                installation = "test";
-                writeReadMe();
+                inquirer.prompt({
+                    message: "Tell users how to install your file.", 
+                    name: "inst"
+                }).then(({inst}) => {
+                    installation = inst;
+                    writeUsage();
+                });
             }
             const writeUsage = () => {
-                usage = "test";
-                writeReadMe();
+                inquirer.prompt({
+                    message: "Describe some use cases for your project",
+                    name: "use"
+                }).then(({use}) =>{
+                    usage = use
+                    writeTests();
+                })
             }
             const writeTests = () => {
-                tests = "test";
-                writeReadMe();
+                inquirer.prompt({
+                    message: "Describe a test case for your project",
+                    name: "testing"
+                }).then(({testing}) => {
+                    tests = testing;
+                    writeFAQ();
+                });
             }
             const writeFAQ = () => {
-                faq = "test";
-                writeReadMe();
+                inquirer.prompt({
+                    message: "Write a FAQ(Frequently Asked Questions) for your project. (If you want it to look good format it like this: * Q: Question * A: Answer)",
+                    name: "faqs"
+                }).then(({faqs}) => {
+                    faq = faqs;
+                    parseCredits(res.data.contributors_url);
+                })
             }
             const parseCredits = contribUrl => {
                 axios.get(contribUrl)
@@ -84,7 +121,7 @@ ${license}
                             creds += `[${user.login}](${user.url})\n`;
                         });
                         credits = creds;
-                        writeReadMe();
+                        parseLicense(res.data.license);
                     })
             }
             const parseLicense = lic => {
@@ -95,11 +132,6 @@ ${license}
                 }
                 writeReadMe();
             }
-            writeInstallation();
-            writeUsage();
-            writeTests();
-            writeFAQ();
-            parseCredits(res.data.contributors_url);
-            parseLicense(res.data.license);
+            writeDescription(res.data.description);
         });
 })
